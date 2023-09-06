2056c6d5f453edd6f1aee244bbdc7f956c91cfc5
Henrique <henriquepinheiroxavier@gmail.com>
2023-09-06 10:00:19 -0300

    feat: menu hamburger

--- main.js
+++ main.js
@@ -8,3 +8,37 @@ function activeAccordion() {
 question.forEach((item) => {
   item.addEventListener('click', activeAccordion);
 });
+
+const tabMenu = document.querySelectorAll('.jstabmenu');
+const tabContent = document.querySelectorAll('.tabcontent');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  })
+})
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+
+
