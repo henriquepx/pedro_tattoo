6590a9d5e4894e450f6a624c8f21c2f2630e8829
Henrique <henriquepinheiroxavier@gmail.com>
2023-09-10 10:54:07 -0300

    feat: footer structure

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-const question = document.querySelectorAll(".cardquestion .question");
-
-function activeAccordion() {
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
-
-question.forEach((item) => {
-  item.addEventListener('click', activeAccordion);
-});
-
-const tabMenu = document.querySelectorAll('.jstabmenu');
-const tabContent = document.querySelectorAll('.tabcontent');
-
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
-  });
-  tabContent[index].classList.add('ativo');
-}
-
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index);
-  })
-})
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".menu");
 
